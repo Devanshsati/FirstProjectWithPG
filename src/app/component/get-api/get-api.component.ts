a093be1ded8bdbf1cf0d1a3d308c622adb6989ca
Devanshsati <thedevanshsati@gmail.com>
2024-09-08 21:11:24 +0530

    getapi

--- src/app/component/get-api/get-api.component.ts
+++ src/app/component/get-api/get-api.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { error } from 'node:console';
 
 @Component({
   selector: 'app-get-api',
@@ -8,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrl: './get-api.component.css'
 })
 export class GetApiComponent {
+  http = inject(HttpClient);
+  customerList: any[] = [];
+  userList: any[] = [];
 
+  getAllUser(){
+    debugger
+    this.http.get("https://jsonplaceholder.typicode.com/users").subscribe((res:any)=>{
+      this.userList = res;
+    });
+  }
+
+  getAllCustomer(){
+    debugger
+    this.http.get("https://projectapi.gerasim.in/api/RealEstate/GetAllCustomers").subscribe((res:any)=>{
+      this.customerList = res.data;
+    },error => {
+      debugger;
+    });
+  }
 }
